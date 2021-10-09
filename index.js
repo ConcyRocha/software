b161e6b220c0ee0930b963846860351c8980cfe8
ConcyRocha <concybarbosa2000@gmail.com>
2021-10-08 20:21:18 -0400

    main

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/camera_index", function(req,res){
     res.sendFile(__dirname + '/camera_index.html')
     
 })
+app.get("/teste", function(req,res){
+    res.sendFile(__dirname + '/teste.html')
+    
+})
 
 app.listen(port,function(){
     console.info('servidor rodando em http://localhost:4000')
